866c3431796d1a1b1dcb8018c6d457e8fa9f7b95
sprudnikov <Roleshio44>
2016-11-23 12:25:34 +0600

    new user - client validation in progress

--- src/admin.php
+++ src/admin.php
@@ -15,16 +15,15 @@
                             minLength: 2,
                             required: true
                         }
-                    }
+                    },
                     highlight: function (element) {
                         $(element).closest('.control-group').removeClass('success').addClass('error');
-                    }
+                    },
                     success: function (element) {
                         element.text('OK!').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
                     }
                 });
-            }
-        );
+            });
     </script>
 </head>
 <body>
